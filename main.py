19e0c17e93f31bb10a22891532202d567d007921
jaco529d <jaco529d@study.sde.dk>
2023-01-27 09:44:18 +0100

    Init microbit and radio

---
+++ main.py
@@ -0,0 +1,27 @@
+from microbit import *
+import radio
+
+radio.config(group=111)
+radio.on()
+#display.scroll("Hello, World!")
+#sleep(100)
+display.show(Image('99999:'
+                   '90009:'
+                   '90709:'
+                   '90009:'
+                   '99999:'))
+
+sleep(1000)
+display.clear
+
+while True:
+    if button_a.was_pressed():
+        display.show('A')
+        radio.send('W')
+    if button_b.was_pressed():
+        display.clear()
+        radio.send('L')
+    
+    message = radio.receive()
+    if message:
+        display.scroll(message)
