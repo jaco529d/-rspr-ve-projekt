588e51c7778aa4ffbbcb105a2ff26411b795bac0
jaco529d <jaco529d@study.sde.dk>
2023-01-31 12:29:11 +0100

    Accelerometer and small

--- main.py
+++ main.py
@@ -6,25 +6,30 @@ radio.config(group=111)
 radio.on()
 #display.scroll("Hello, World!")
 #sleep(100)
-display.show(Image('99999:'
-                   '90009:'
-                   '90709:'
-                   '90009:'
-                   '99999:'))
+'''display.show(Image.HEART)
 
 sleep(1000)
-display.clear()
+display.clear()'''
+button_b.was_pressed()
+button_a.was_pressed()
 
 while True:
+    display.scroll('T')
     if button_a.was_pressed():
-        display.show('A')
+        display.show(Image('99999:'
+                           '99999:'
+                           '99999:'
+                           '99999:'
+                           '99999'))
         radio.send('W')
+        sleep(1000)
+        towerGame(display, button_a, button_b)
     if button_b.was_pressed():
         display.clear()
         radio.send('L')
     
     #--Tower game--#
-    towerGame(display)
+    
 
     #--Radio Control--#
     message = radio.receive()
--- tower.py
+++ tower.py
@@ -1,5 +1,33 @@
-def towerGame(display):
+from microbit import *
+
+def towerGame(display, button_a, button_b):
     running = True
+    xpos = 0
+    xdir = 'right'
     while running == True:
-        display.scroll('tower')
+        display.clear()
+
+        #color new pixel
+        display.set_pixel(xpos,2,9)
         
+        
+        if button_b.was_pressed():
+            running = False
+        
+        #pixel pos
+        if xdir == 'right':
+            xpos = xpos + 1
+        elif xdir == 'left':
+            xpos = xpos - 1
+
+        if xpos == 4:
+            xdir = 'left'
+        elif xpos == 0:
+            xdir = 'right'
+
+        #tower stacking
+        if accelerometer.was_gesture('shake'):
+            display.set_pixel(0,0,9)
+
+        sleep(100)
+
